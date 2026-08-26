acdfa70ae50cb0b11f244aaad6cc5f8ec68c8d1e
Ashmizan <ashfiya.sharin.mizan@gmail.com>
2026-08-26 16:53:05 +0300

    Add main.jsx to connect React to the page

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+Connects react to the page
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
